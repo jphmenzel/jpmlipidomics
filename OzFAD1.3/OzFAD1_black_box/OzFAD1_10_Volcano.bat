56f6e431ddf491ee615b8ac24918ec28c917db2a
Jan Philipp Menzel <74584759+jphmenzel@users.noreply.github.com>
2023-01-13 14:29:16 +1000

    Add files via upload

---
+++ OzFAD1.3/OzFAD1_black_box/OzFAD1_10_Volcano.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+rem ECHO ------- OzFAD1 step 11 -------
+
+rem Venn inspired barchart
+"python.exe" "%~dp0\OzFAD1_py\OzFAD_py_tools\OzFAD1_P_value_heatmap_data.py"
+
+:END
+ENDLOCAL
+PAUSE
---
+++ OzFAD1.3/OzFAD1_black_box/OzFAD1_11_Venn_Barchart.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+rem ECHO ------- OzFAD1 step 11 -------
+
+rem Venn inspired barchart
+"python.exe" "%~dp0\OzFAD1_py\OzFAD_py_tools\OzFAD1_Venn_barchart.py"
+
+:END
+ENDLOCAL
+PAUSE
